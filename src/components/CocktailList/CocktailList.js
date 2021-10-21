8e41893c36b242b019696de5b3400b8c596edc03
Nguyen Thanh Kiet <kiet13.itus@gmail.com>
2021-10-21 14:35:26 +0700

    Add CocktailList components to show fetch data

--- src/components/CocktailList/CocktailList.js
+++ src/components/CocktailList/CocktailList.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import styles from './CocktailList.module.scss'
+import Cocktail from '../Cocktail/Cocktail'
 
-export default function CocktailList() {
+export default function CocktailList({ cocktails }) {
+  const cocktailList = cocktails.map(item => 
+    <Cocktail key={item.id} item={item} />
+  )
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <h1 className={styles.CocktailsHeading}>Cocktails</h1>
+      <div className={styles.CocktailList}>
+        {cocktailList}
+      </div>
+    </>
   )
 }
